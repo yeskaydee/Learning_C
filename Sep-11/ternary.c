1bcb835dd507853d0983282517dcc56a10b06444
Santosh Kumar Doodala <84093797+yeskaydee@users.noreply.github.com>
2021-09-11 23:19:38 +0530

    Add files via upload

---
+++ Sep-11/ternary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a ;
+    printf("Enter the valude of a");
+    scanf("%d", &a);
+
+    (a<=5)? printf("a is less than 5"):printf("a is not less5 than 5");
+
+    return 0;
+}
